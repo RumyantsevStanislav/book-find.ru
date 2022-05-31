8079ca7b24e5d509538376c0f1a1a696a471563a
Румянцев Станислав <stasrum@bk.ru>
2022-05-31 21:13:22 +0300

    Added LabirintParser, refactored Main class.

---
+++ backend/html-parsers/src/main/java/LabirintParser.java
@@ -0,0 +1,171 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jsoup.Connection;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import server.entities.*;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LabirintParser {
+
+    private static final Logger logger = LogManager.getLogger();
+    private final Document document;
+
+    LabirintParser(Connection connection) throws IOException {
+        this.document = connection.execute().parse();
+    }
+
+    public Book getBook() {
+        Book book = new Book();
+        book.setLabirintId(getLabirintId(document));
+        book.setTitle(getTitle(document));
+        book.setAuthors(getAuthors(document));
+        book.setDescription(getDescription(document));
+        book.setPages(getPages(document));
+        book.setEstimation(getEstimation(document));
+        book.setPublisher(getPublisher(document));
+        book.setSeries(getSeries(document));
+        book.setCategories(getCategories(document));
+        book.setGenre(getGenre(document));
+        book.setPrice(0);
+        return book;
+    }
+
+    private Long getLabirintId(Document document) {
+        Long labirintId = parseLong(onlyDigits(getElementText(document.selectFirst("div.articul"))));
+        logger.info("Correct get labirintId: {}", labirintId);
+        return labirintId;
+    }
+
+    private String getTitle(Document document) {
+        String title = getElementText(document.selectFirst("div#product-title > h1"));
+        logger.info("Correct get title: {}", title);
+        return title;
+    }
+
+    private Set<Author> getAuthors(Document document) {
+        Set<Author> authorsList = new HashSet<>();
+        Elements authorElements = document.select("div.authors");
+        for (Element authorElement : authorElements) {
+            Long authorId = parseLong(onlyDigits(getElementAttribute(authorElement.selectFirst("a"), "abs:href")));
+            String[] authorNameAndRole = authorElement.text().split(": ", 2);
+            String authorRole = authorNameAndRole[0];
+            String authorName = authorNameAndRole[1];
+            Author author = new Author();
+            author.setLabirintId(authorId);
+            author.setName(authorName);
+            author.setRole(authorRole);
+            authorsList.add(author);
+        }
+        logger.info("Correct get authors list {}", authorsList);
+        return authorsList;
+    }
+
+    private String getDescription(Document document) {
+        Element fullAnnotation = document.getElementById("product-about");
+        String description = fullAnnotation == null ? "" : getElementText(fullAnnotation.selectFirst("p"));
+        logger.info("Correct get description: {}", description);
+        return description;
+    }
+
+    private Float getEstimation(Document document) {
+        Float estimation = parseFloat(getElementText(document.getElementById("rate")));
+        logger.info("Correct get estimation: {}", estimation);
+        return estimation;
+    }
+
+    private Integer getPages(Document document) {
+        Integer pages = parseInt(getElementAttribute(document.selectFirst("div.pages2 > span"), "data-pages"));
+        logger.info("Correct get pages: {}", pages);
+        return pages;
+    }
+
+    private Series getSeries(Document document) {
+        Series series = new Series();
+        Element seriesElement = document.selectFirst("div.series > a");
+        String title = getElementText(seriesElement);
+        Long seriesId = parseLong(onlyDigits(getElementAttribute(seriesElement, "href")));
+        series.setTitle(title);
+        series.setLabirintId(seriesId);
+        logger.info("Correct get series {}", series);
+        return series;
+    }
+
+    private Publisher getPublisher(Document document) {
+        Publisher publisher = new Publisher();
+        Element publisherElement = document.selectFirst("div.publisher > a");
+        Long publisherId = parseLong(onlyDigits(getElementAttribute(publisherElement, "href")));
+        String publisherTitle = getElementText(publisherElement);
+        publisher.setLabirintId(publisherId);
+        publisher.setTitle(publisherTitle);
+        logger.info("Correct get publisher {}", publisher);
+        return publisher;
+    }
+
+    private Set<Category> getCategories(Document document) {
+        Set<Category> categoryList = new HashSet<>();
+        Elements categoryElements = document.select("div.genre > a");
+        for (Element categoryElement : categoryElements) {
+            Long categoryId = parseLong(onlyDigits(categoryElement.attr("href")));
+            String categoryTitle = categoryElement.attr("data-event-content");
+            Category category = new Category();
+            category.setLabirintId(categoryId);
+            category.setTitle(categoryTitle);
+            categoryList.add(category);
+        }
+        logger.info("Correct get category list {}", categoryList);
+        return categoryList;
+    }
+
+    private Genre getGenre(Document document) {
+        String path = getElementText(document.getElementById("thermometer-books"));
+        Genre genre = new Genre();
+        genre.setPath(path);
+        logger.info("Correct get genre {}", genre);
+        return genre;
+    }
+
+    private Long parseLong(String element) {
+        try {
+            return Long.parseLong(element);
+        } catch (NumberFormatException numberFormatException) {
+            logger.warn("Fail to parseLong element: {}", element);
+            return 0L;
+        }
+    }
+
+    private Integer parseInt(String element) {
+        try {
+            return Integer.parseInt(element);
+        } catch (NumberFormatException numberFormatException) {
+            logger.warn("Fail to parseInt element: {}", element);
+            return 0;
+        }
+    }
+
+    private Float parseFloat(String element) {
+        try {
+            return Float.parseFloat(element);
+        } catch (NumberFormatException numberFormatException) {
+            logger.warn("Fail to parseFloat element: {}", element);
+            return 0F;
+        }
+    }
+
+    private String onlyDigits(String element) {
+        return element.replaceAll("\\D+", "");
+    }
+
+    private String getElementText(Element element) {
+        return element == null ? "" : element.text();
+    }
+
+    private String getElementAttribute(Element element, String attribute) {
+        return element == null ? "" : element.attr(attribute);
+    }
+
+}
--- backend/html-parsers/src/main/java/Main.java
+++ backend/html-parsers/src/main/java/Main.java
@@ -1,29 +1,53 @@
-import org.apache.http.NameValuePair;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.entity.ContentType;
-import org.apache.http.message.BasicNameValuePair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import org.jsoup.nodes.Element;
+import server.entities.Book;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Objects;
+import java.util.*;
 
 public class Main {
-    private static final String IMAGE_DESTINATION_FOLDER = "F:/BooksImages8";
+    private static final String IMAGE_DESTINATION_FOLDER = "F:/BooksImages16";
     private static final String PATH = "https://www.labirint.ru/books/";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final Logger logger = LogManager.getLogger();
 
-    public static void main(String[] args) throws IOException {
-        for (int i = 435623; i <= 450000; i++) {
+    public static void main(String[] args) {
+        saveImages();
+        saveBooks();
+    }
+
+    private static void saveBooks() {
+        for (int i = 1; i < 855000; i++) {
+            var url = "https://www.labirint.ru/books/" + i;
+            Connection connection = Jsoup.connect(url).userAgent("Chrome/81.0.4044.138");
+            try {
+                LabirintParser labirintParser = new LabirintParser(connection);
+                Book book = labirintParser.getBook();
+                final Content postResult = Request.Post("http://localhost:8189/book-find/api/v1/books")
+                        .bodyString(objectMapper.writeValueAsString(book), ContentType.APPLICATION_JSON)
+                        .execute().returnContent();
+                System.out.println(postResult.asString());
+            } catch (IOException ioException) {
+                logger.error("Unable to create LabirintParser");
+            }
+        }
+    }
+
+    private static void saveImages() {
+        for (int i = 1; i <= 855000; i++) {
             String url = PATH + i;
             Connection connection = Jsoup.connect(url).userAgent("Chrome/81.0.4044.138");
             try {
@@ -34,80 +58,31 @@ public class Main {
                 String strImageURL = image.attr("abs:data-src");
                 downloadImage(strImageURL, articul);
             } catch (Exception e) {
-                //System.out.println("Не удалось скачать " + i);
+                logger.error("Не удалось скачать " + i);
             }
         }
-
-//        Elements author = document.select("div.authors > a");
-//        String authorName =author.text();
-        //> вложенные теги
-        //Elements titleElem = document.select("head > title");
-        //System.out.println(author);
-//        Book book = new Book();
-//        book.setTitle("title");
-//        //book.setCategories("category");
-//        book.setDescription("description");
-//        book.setPrice(123F);
-
-
-//        final Content getResult = Request.Get("http://jsonplaceholder.typicode.com/posts?_limit=10")
-//                .execute().returnContent();
-//        System.out.println(getResult.asString());
-
-        final Collection<NameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("title", "foo"));
-        params.add(new BasicNameValuePair("body", "bar"));
-        params.add(new BasicNameValuePair("userId", "1"));
-
-//        final Content postResultForm = Request.Post("http://jsonplaceholder.typicode.com/posts")
-//                .bodyForm(params, Charset.defaultCharset())
-//                .execute().returnContent();
-//        System.out.println(postResultForm.asString());
-
-//        final Content postResult = Request.Post("http://localhost:8189/book-find/api/v1/books")
-//                .bodyString(objectMapper.writeValueAsString(book), ContentType.APPLICATION_JSON)
-//                .execute().returnContent();
-//        System.out.println(postResult.asString());
-
-//        Elements paragraphs = document.getElementsByTag("p");
-//        for (Element paragraph : paragraphs) {
-//            System.out.println(paragraph.text());
-//        }
     }
 
     private static void downloadImage(String strImageURL, String articul) {
-
         //get file name from image path
         String strImageName = articul + ".jpg";
         //strImageURL.substring( strImageURL.lastIndexOf("/") + 1 );
-
         System.out.println("Saving: " + strImageName + ", from: " + strImageURL);
-
         try {
-
             //open the stream from URL
             URL urlImage = new URL(strImageURL);
             InputStream in = urlImage.openStream();
-
             byte[] buffer = new byte[4096];
-            int n = -1;
-
+            int n;
             OutputStream os =
                     new FileOutputStream(IMAGE_DESTINATION_FOLDER + "/" + strImageName);
-
-            //write bytes to the output stream
             while ((n = in.read(buffer)) != -1) {
                 os.write(buffer, 0, n);
             }
-
-            //close the stream
             os.close();
-
-            //System.out.println("Image saved");
-
+            System.out.println("Image saved");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
