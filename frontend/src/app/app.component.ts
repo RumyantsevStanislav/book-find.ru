1fe226355296cacf29bd55063cfd1abd49887a5f
Румянцев Станислав <stasrum@bk.ru>
2022-06-06 21:49:12 +0300

    0.1.0 addition for previous commit - app files

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {BookService} from "./services/books-service/books.service";
+import {Book} from "./shared/interfaces";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+
+export class AppComponent implements OnInit {
+
+
   constructor() {
-    this.title = 'Spring Boot - Angular Application';
   }
+
+  ngOnInit() {
+
+  }
+//   addBook(): void {
+//     this.router.navigate(['add-book'])
+//       .then((e) => {
+//         if (e) {
+//           console.log("Navigation is successful!");
+//         } else {
+//           console.log("Navigation has failed!");
+//         }
+//       });
+//   };
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BookComponent } from './book/book.component';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from "@angular/forms";
+import {BookPageComponent} from "./components/book-page/book-page.component";
+import {AppRoutingModule} from "./app-routing.module";
+import {BookService} from "./services/books-service/books.service";
+import { MainComponent } from './shared/main/main.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import {BookComponent} from "./shared/book/book.component";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
+    BookPageComponent,
+    MainComponent,
+    HomePageComponent,
     BookComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    SharedModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [BookService, BookPageComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
