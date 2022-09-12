dcc296a1ad243a983451eddb24bbbadb0de97785
Румянцев Станислав <stasrum@bk.ru>
2022-09-12 22:33:40 +0300

    0.4.0 added saving user test in service layer

--- backend/server/src/test/java/server/services/UsersServiceTests.java
+++ backend/server/src/test/java/server/services/UsersServiceTests.java
@@ -2,51 +2,66 @@ package server.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import server.configs.PasswordEncoder;
 import server.entities.User;
+import server.entities.dtos.SystemUser;
 import server.repositories.UsersRepository;
 import server.utils.TestUsers;
 
 import java.util.Optional;
 
-@SpringBootTest
-@ActiveProfiles("test")
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+
+@ExtendWith(MockitoExtension.class)
 public class UsersServiceTests {
-    @Autowired
+    @InjectMocks
     private UsersService usersService;
 
-    @MockBean
+    @Mock
     private UsersRepository usersRepository;
+
+    @Mock
+    private RolesService rolesService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @BeforeEach
     void setUp() {
         User userFromDb = TestUsers.getUser();
-        Mockito.doReturn(Optional.of(userFromDb)).when(usersRepository).findByPhone(TestUsers.USER_PHONE);
-        Mockito.doReturn(Optional.of(userFromDb)).when(usersRepository).findByEmail(TestUsers.USER_EMAIL);
+        Mockito.lenient().doReturn(Optional.of(userFromDb)).when(usersRepository).findByEmail(TestUsers.USER_EMAIL);
+        Mockito.lenient().doReturn(Optional.of(userFromDb)).when(usersRepository).findByPhone(TestUsers.USER_PHONE);
     }
 
     @Test
+    @DisplayName("Get user by phone or email is success")
     public void getByPhoneOrEmailTest() {
         Optional<User> user;
         user = usersService.getUserByPhone(TestUsers.USER_PHONE);
         Assertions.assertTrue(user.isPresent());
         user = usersService.getUserByEmail(TestUsers.USER_EMAIL);
         Assertions.assertTrue(user.isPresent());
-        Mockito.verify(usersRepository, Mockito.times(1)).findByPhone(ArgumentMatchers.eq(TestUsers.USER_PHONE));
-        Mockito.verify(usersRepository, Mockito.times(1)).findByEmail(ArgumentMatchers.any(String.class));
+        Mockito.verify(usersRepository, Mockito.times(1)).findByPhone(eq(TestUsers.USER_PHONE));
+        Mockito.verify(usersRepository, Mockito.times(1)).findByEmail(any(String.class));
 
         usersService.getUserByPhoneOrEmail(TestUsers.USER_PHONE);
-        Mockito.verify(usersRepository, Mockito.times(2)).findByPhone(ArgumentMatchers.eq(TestUsers.USER_PHONE));
+        Mockito.verify(usersRepository, Mockito.times(2)).findByPhone(eq(TestUsers.USER_PHONE));
         usersService.getUserByPhoneOrEmail(TestUsers.USER_EMAIL);
-        Mockito.verify(usersRepository, Mockito.times(2)).findByEmail(ArgumentMatchers.any(String.class));
+        Mockito.verify(usersRepository, Mockito.times(2)).findByEmail(any(String.class));
     }
 
     @Test
@@ -59,15 +74,14 @@ public class UsersServiceTests {
         Assertions.assertTrue(userDetails.getAuthorities().stream().allMatch(grantedAuthority -> TestUsers.getRoleUser().getAuthority().contains(grantedAuthority.getAuthority())));
     }
 
-    //    @Test
-    //    public void saveTest() {
-    //        SystemUser systemUser = TestUsers.getSystemUser();
-    //        Mockito.doReturn(Optional.of(TestUsers.getNewUser())).when(usersRepository).save(ArgumentMatchers.any(User.class));
-    //        Mockito.doReturn(TestUsers.USER_PASSWORD_ENCODED).when(passwordEncoder.getPasswordEncoder().encode(systemUser.getPassword()));
-    //        Mockito.doReturn(Role.Privilege.ROLE_USER).when(rolesService.findByName(ArgumentMatchers.any(String.class)));
-    //        User savedUser = usersService.save(systemUser);
-    //        Assertions.assertEquals(savedUser.getPhone(), systemUser.getPhone());
-    //        Assertions.assertEquals(savedUser.getEmail(), systemUser.getEmail());
-    //        Assertions.assertEquals(savedUser.getPassword(), TestUsers.USER_PASSWORD_ENCODED);
-    //    }
+    @Test
+    public void saveTest() {
+        SystemUser systemUser = TestUsers.getSystemUser();
+        Mockito.doReturn(TestUsers.getUser()).when(usersRepository).save(any(User.class));
+        Mockito.doReturn(bCryptPasswordEncoder).when(passwordEncoder).getPasswordEncoder();
+        Mockito.doReturn(TestUsers.USER_PASSWORD_ENCODED).when(bCryptPasswordEncoder).encode(systemUser.getPassword());
+        Mockito.doReturn(TestUsers.getRoleUser()).when(rolesService).findByName(any(String.class));
+        User savedUser = usersService.save(systemUser);
+        Assertions.assertEquals(savedUser.getPassword(), TestUsers.USER_PASSWORD_ENCODED);
+    }
 }
