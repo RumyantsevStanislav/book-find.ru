b3423edba2d391c16e91715f0fe8b5d353393c66
Румянцев Станислав <stasrum@bk.ru>
2022-09-18 15:39:59 +0300

    0.4.0 merge AuthController.java to UsersController

--- backend/server/src/main/java/server/controllers/UsersController.java
+++ backend/server/src/main/java/server/controllers/UsersController.java
@@ -4,11 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import server.configs.JwtTokenUtil;
 import server.entities.User;
 import server.entities.dtos.ApiMessage;
+import server.entities.dtos.JwtRequest;
+import server.entities.dtos.JwtResponse;
 import server.entities.dtos.SystemUser;
 import server.exceptions.AttributeNotValidException;
 import server.exceptions.ElementAlreadyExistsException;
@@ -20,7 +27,15 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UsersController {
+    private final JwtTokenUtil jwtTokenUtil;
+    private final AuthenticationManager authenticationManager;
     private UsersService usersService;
+
+    public UsersController(UsersService usersService, JwtTokenUtil jwtTokenUtil, AuthenticationManager authenticationManager) {
+        this.usersService = usersService;
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.authenticationManager = authenticationManager;
+    }
 
     @Autowired
     public void setUserService(UsersService usersService) {
@@ -46,4 +61,24 @@ public class UsersController {
         usersService.save(systemUser);
         return new ResponseEntity<>(new ApiMessage("Вы успешно зарегистрированы!"), HttpStatus.CREATED);
     }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping("/auth")
+    public ResponseEntity<JwtResponse> createAuthToken(@RequestBody JwtRequest authRequest) throws Exception {
+        try {
+            authenticate(authRequest.getUsername(), authRequest.getPassword());
+        } catch (BadCredentialsException ex) {
+            //return new ResponseEntity<>(new InfoResponse("Incorrect username or password"), HttpStatus.UNAUTHORIZED);
+        }
+
+        UserDetails userDetails = usersService.loadUserByUsername(authRequest.getUsername());
+
+        String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void authenticate(String username, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    }
+
 }
