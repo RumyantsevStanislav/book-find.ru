b4fe620a40992c2525d409e20374119923fe43a4
Румянцев Станислав <stasrum@bk.ru>
2022-09-18 13:54:13 +0300

    0.4.0 changed ServerApplicationTests

--- backend/server/src/test/java/server/ServerApplicationTests.java
+++ backend/server/src/test/java/server/ServerApplicationTests.java
@@ -1,13 +1,20 @@
 package server;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ServerApplicationTests {
+    @Autowired
+    private ServerApplication serverApplication;
 
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(serverApplication);
     }
 
 }
