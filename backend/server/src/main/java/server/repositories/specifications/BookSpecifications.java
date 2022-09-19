643c53f2d1f9dd7023ac96b159f57007ac5ea0d3
Румянцев Станислав <stasrum@bk.ru>
2022-09-19 22:04:13 +0300

    0.4.0 reformat BookSpecifications, fixed misprint in BookFilter

--- backend/server/src/main/java/server/repositories/specifications/BookSpecifications.java
+++ backend/server/src/main/java/server/repositories/specifications/BookSpecifications.java
@@ -5,12 +5,14 @@ import org.springframework.data.jpa.domain.Specification;
 import server.entities.Book;
 
 public class BookSpecifications {
-    public static Specification<Book> priceGreaterOrEqualsThen(int minPrice){
-        return  (Specification<Book>) (root,criteriaQuery, criteriaBuilder) ->criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice);
+    public static Specification<Book> priceGreaterOrEqualsThen(int minPrice) {
+        return (Specification<Book>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice);
     }
+
     public static Specification<Book> priceLesserOrEqualsThen(int maxPrice) {
         return (Specification<Book>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("price"), maxPrice);
     }
+
     public static Specification<Book> titleLike(String title) {
         return (Specification<Book>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("title"), String.format("%%%s%%", title));
     }
@@ -18,8 +20,8 @@ public class BookSpecifications {
     public static Specification<Book> categoryIs(Category category) {
         return (Specification<Book>) (root, criteriaQuery, criteriaBuilder) -> {
 
-//            Join join = root.join("categories");
-//            return criteriaBuilder.equal(join.get("id"), category.getId());
+            //            Join join = root.join("categories");
+            //            return criteriaBuilder.equal(join.get("id"), category.getId());
             return criteriaBuilder.isMember(category, root.get("categories"));
         };
     }
--- backend/server/src/main/java/server/utils/BookFilter.java
+++ backend/server/src/main/java/server/utils/BookFilter.java
@@ -1,9 +1,9 @@
 package server.utils;
 
-import server.entities.Category;
 import lombok.Getter;
 import org.springframework.data.jpa.domain.Specification;
 import server.entities.Book;
+import server.entities.Category;
 import server.repositories.specifications.BookSpecifications;
 
 import java.util.List;
@@ -14,17 +14,17 @@ public class BookFilter {
     private Specification<Book> spec;
     private StringBuilder filterDefinition;
 
-    public BookFilter (Map<String, String> map, List<Category> categories){
+    public BookFilter(Map<String, String> map, List<Category> categories) {
         this.spec = Specification.where(null);
         this.filterDefinition = new StringBuilder();
-        if(map.containsKey("min_price") && !map.get("min_price").isEmpty()){
+        if (map.containsKey("min_price") && !map.get("min_price").isEmpty()) {
             int minPrice = Integer.parseInt(map.get("min_price"));
             spec = spec.and(BookSpecifications.priceGreaterOrEqualsThen(minPrice));
             filterDefinition.append("&min_price=").append(minPrice);
         }
-        if(map.containsKey("max_price") && !map.get("max_price").isEmpty()){
+        if (map.containsKey("max_price") && !map.get("max_price").isEmpty()) {
             int maxPrice = Integer.parseInt(map.get("max_price"));
-            spec = spec.and(BookSpecifications.priceGreaterOrEqualsThen(maxPrice));
+            spec = spec.and(BookSpecifications.priceLesserOrEqualsThen(maxPrice));
             filterDefinition.append("&max_price=").append(maxPrice);
         }
         if (map.containsKey("title") && !map.get("title").isEmpty()) {
