8768e6b97794363826269ce3ad6e2dbd3d71e108
Румянцев Станислав <stasrum@bk.ru>
2022-09-19 19:35:26 +0300

    0.4.0 added page of books test (without category filter)

--- backend/server/src/test/java/server/repositories/BooksRepositoryTests.java
+++ backend/server/src/test/java/server/repositories/BooksRepositoryTests.java
@@ -5,13 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import server.entities.Book;
+import server.entities.Category;
+import server.utils.BookFilter;
 import server.utils.TestBooks;
 
 import javax.persistence.PersistenceException;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -26,13 +29,27 @@ public class BooksRepositoryTests {
     public void bookRepositoryTest() {
         Optional<Book> book = booksRepository.findByIsbn(TestBooks.ISBN);
         Assertions.assertTrue(book.isPresent());
+
         List<Book> bookList = booksRepository.findByTitle(TestBooks.BOOK_TITLE);
         Assertions.assertEquals(1, bookList.size());
+
         booksRepository.deleteByIsbn(TestBooks.ISBN);
         List<Book> allBooks = booksRepository.findAll();
         Assertions.assertEquals(1, allBooks.size());
+
         Book existingBook = TestBooks.getBook();
         Assertions.assertThrows(PersistenceException.class, () -> entityManager.persist(existingBook));
+    }
+
+    @Test
+    public void getPageBooksTest() {
+        Map<String, String> parameters = Map.of("min_price", "0");
+        List<Category> categories = TestBooks.getCategories().stream().toList();
+        BookFilter bookFilter = new BookFilter(parameters, null);
+        Page<Book> bookPage = booksRepository.findAll(bookFilter.getSpec(), PageRequest.of(0, 1));
+        Assertions.assertEquals(bookPage.getTotalElements(), 2);
+        Assertions.assertEquals(bookPage.getTotalPages(), 2);
+        Assertions.assertEquals(bookPage.getContent().size(), 1);
     }
 
     @Test
