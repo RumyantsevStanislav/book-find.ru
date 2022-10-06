8952f66b315b3b923f13c3b3394f176f88da734e
Румянцев Станислав <stasrum@bk.ru>
2022-10-06 15:40:30 +0300

    0.5.0 added ng-bootstrap, boostrap-link, app.component.scss

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent implements OnInit {
@@ -15,6 +15,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
 
   }
+
 //   addBook(): void {
 //     this.router.navigate(['add-book'])
 //       .then((e) => {
