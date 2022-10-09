f0d04955ffa73af000df96a602fa3628619bdde3
Румянцев Станислав <stasrum@bk.ru>
2022-10-09 09:05:58 +0300

    0.5.0 flexbox style to main-menu component

--- frontend/src/app/shared/main/main.component.ts
+++ frontend/src/app/shared/main/main.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
 
